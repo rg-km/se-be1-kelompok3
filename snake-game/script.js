cb2f577584a5f96bc38a41bdac4b6d34021d3178
Putu Hitapriya Adyota <putuhitapriyaadyota@gmail.com>
2022-03-12 21:12:25 +0800

    feat: Add gameover sound and change life image

--- snake-game/script.js
+++ snake-game/script.js
@@ -38,9 +38,9 @@ function initSnake(color) {
     ...initHeadAndBody(),
     direction: initDirection(),
     score: 0,
-    lifepos :[{ x: 0, y: 0 },
-    {x:1, y: 0},
-    {x:2, y: 0}
+    lifepos :[{ x: 1, y: 1 },
+    {x: 2, y: 1},
+    {x: 3, y: 1}
     ]
   }
 }
@@ -207,6 +207,7 @@ function checkGameover(snakes) {
   }
  
   if(isGameover){
+    document.getElementById('gameover').play()
     alert('Game Over!')
     snake = initSnake('green')
   }
